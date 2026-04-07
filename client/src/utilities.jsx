4f9f7410125565fc9243a0787cbb69ef3c192b96
fravila08 <fr4v1l4@gmail.com>
2026-04-07 10:08:37 -0700

    updated axios url call

--- client/src/utilities.jsx
+++ client/src/utilities.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: '/api/v1/'
+    baseURL: 'http://3.138.247.77/api/v1/'
 })
 
 export const deleteATask = async( id ) => {
