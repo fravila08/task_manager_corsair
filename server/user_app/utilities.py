467254521febb61fb516f2b293d030e1ee1f57a2
fravila08 <fr4v1l4@gmail.com>
2026-04-10 14:23:53 -0700

    updated cookies for jwt auth

--- server/user_app/utilities.py
+++ server/user_app/utilities.py
@@ -1,15 +1,21 @@
-from rest_framework.authentication import TokenAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework import exceptions
 
-class CookieAuthentication(TokenAuthentication):
+class CookieAuthentication(JWTAuthentication):
         
         def get_auth_cookie(self, request):
-             return request.COOKIES.get('token')
+             return request.COOKIES.get('access')
     
         def authenticate(self, request):
-            auth = self.get_auth_cookie(request)
+            access_token = self.get_auth_cookie(request)
 
-            if not auth:
+            if not access_token:
                   raise exceptions.AuthenticationFailed("Authentication Cookie is not present")
             
-            return self.authenticate_credentials(auth)
+            try:
+                  validate_token = self.get_validated_token(access_token)
+                  return self.get_user(validate_token), validate_token
+            except (InvalidToken, TokenError) as e:
+                  raise exceptions.AuthenticationFailed(str(e))
+            
